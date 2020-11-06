408247ff20ef44c9dc2477c6b26fd22a7d0db2e4
Izurii <46232520+Izurii@users.noreply.github.com>
2020-11-06 17:59:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,6 @@ def predict_api():
     output = prediction[0]
     return jsonify(output)
 
-#if __name__ == "__main__":
-#    app.run(debug=True)
-
+port = int(os.environ.get('PORT', 5000))
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=port)
